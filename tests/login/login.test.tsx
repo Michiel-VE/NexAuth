6aa5f6f8d7042d39309afe666831825c283d9a8f
Michiel-ve <michiel.van.eynde123@gmail.com>
2025-01-14 12:53:41 +0200

    fix test login

--- tests/login/login.test.tsx
+++ tests/login/login.test.tsx
@@ -1,6 +1,6 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import {act, render, screen, waitFor} from "@testing-library/react";
 import LoginPage from "@/app/login/page"; // Adjust import path if necessary
-import { login } from '@/app/api/action/auth';
+import {login} from '@/app/api/action/auth';
 import {fireEvent} from "@testing-library/dom"; // Mock the login function
 
 // Mock the login function
@@ -12,33 +12,33 @@ jest.mock('../../src/app/api/action/auth', () => ({
 global.fetch = jest.fn(() =>
     Promise.resolve({
         json: () => Promise.resolve({
-            google: { id: 'google', name: 'Google' },
-            github: { id: 'github', name: 'GitHub' },
+            google: {id: 'google', name: 'Google'},
+            github: {id: 'github', name: 'GitHub'},
         }),
     })
 );
 
 describe('Login', () => {
-    beforeEach(() => {
+    beforeEach(async () => {
         jest.clearAllMocks();
+        await act(async () => {
+            render(<LoginPage/>);
+        });
     });
 
-    it('Should render loading state when providers are being fetched', async () => {
-        render(<LoginPage />);
+    it('Should render loading state when providers are being fetched', () => {
+        render(<LoginPage/>);
 
         expect(screen.getByText('Loading...')).toBeInTheDocument();
     });
 
     it('Should render provider buttons when loaded', async () => {
-        render(<LoginPage />);
 
         await waitFor(() => expect(screen.getByText('Sign in with Google')).toBeInTheDocument());
         await waitFor(() => expect(screen.getByText('Sign in with GitHub')).toBeInTheDocument());
     });
 
     it('Should call login function when a provider button is clicked', async () => {
-        render(<LoginPage />);
-
         await waitFor(() => screen.getByText('Sign in with Google'));
 
         fireEvent.click(screen.getByText('Sign in with Google'));
